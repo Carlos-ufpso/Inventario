19453c67233d6205d53a8016e6e861f89f6eaabf
Carlos-ufpso <cjperezma@ufpso.edu.co>
2024-09-27 22:03:26 -0500

    Add files via upload

---
+++ Inventario.java
@@ -0,0 +1,146 @@
+// Carlos Jhosef Pérez Maldonado 192364
+// Valerin Fernanda Hernandez Prada 1923
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Inventario {
+    // Se crea unas constantes para el menú
+    private static final String OPCION_SALIR = "0";
+    private static final String OPCION_AGREGAR = "1";
+    private static final String OPCION_ELIMINAR = "2";
+    private static final String OPCION_MODIFICAR = "3";
+    private static final String OPCION_VER_INVENTARIO = "4";
+    
+    public static void main(String[] args) {
+        // Se crea un objeto Scanner para leer la entrada del usuario  
+        Scanner scanner = new Scanner(System.in);
+
+        // Se crea variables para almacenar datos de productos
+        ArrayList<String> productos = new ArrayList<>();
+        ArrayList<Integer> cantidades = new ArrayList<>();
+        ArrayList<Double> precios = new ArrayList<>();
+
+        // Se hace un bucle donde muestre un menú para interactuar con el inventario
+        String opcion;
+        do {
+            // Se hace un menú de opciones
+            System.out.println("\nMenú de Inventario:");
+            System.out.println("1. Agregar un producto");
+            System.out.println("2. Eliminar un producto");
+            System.out.println("3. Modificar un producto");
+            System.out.println("4. Ver el inventario");
+            System.out.println("0. Salir del inventario");
+            System.out.print("Elige una opción: ");
+            opcion = scanner.next();
+
+            switch (opcion) {
+                case OPCION_AGREGAR:
+                    agregarProducto(scanner, productos, cantidades, precios);
+                    break;
+                case OPCION_ELIMINAR:
+                    eliminarProducto(scanner, productos, cantidades, precios);
+                    break;
+                case OPCION_MODIFICAR:
+                    modificarProducto(scanner, productos, cantidades, precios);
+                    break;
+                case OPCION_VER_INVENTARIO:
+                    mostrarInventario(productos, cantidades, precios);
+                    break;
+                case OPCION_SALIR:
+                    System.out.println("Saliendo del sistema...");
+                    break;
+                default:
+                    System.out.println("Opción no válida, intente de nuevo.");
+            }
+        } while (!opcion.equals(OPCION_SALIR));
+    }
+
+    // Esta la función para agregar un nuevo producto
+    private static void agregarProducto(Scanner scanner, ArrayList<String> productos, ArrayList<Integer> cantidades, ArrayList<Double> precios) {
+        System.out.println("Ingrese el nombre del nuevo producto: ");
+        String nombreProducto = scanner.next();
+        System.out.println("Ingrese la cantidad del producto: ");
+        int cantidadProducto = scanner.nextInt();
+        System.out.println("Ingrese el precio del producto: ");
+        double precioProducto = scanner.nextDouble();
+
+        productos.add(nombreProducto);
+        cantidades.add(cantidadProducto);
+        precios.add(precioProducto);
+
+        System.out.println("Producto agregado exitosamente");
+    }
+
+    // Esta es la función para eliminar un producto
+    private static void eliminarProducto(Scanner scanner, ArrayList<String> productos, ArrayList<Integer> cantidades, ArrayList<Double> precios) {
+        System.out.println("Ingrese el número del producto a eliminar: ");
+        mostrarProductos(productos);
+        int index = scanner.nextInt() - 1;  // Se resta 1 para usarlo como índice
+
+        if (index >= 0 && index < productos.size()) {
+            productos.remove(index);
+            cantidades.remove(index);
+            precios.remove(index);
+            System.out.println("Producto eliminado exitosamente");
+        } else {
+            System.out.println("Número de producto inválido");
+        }
+    }
+
+    // Esta es la función para modificar un producto
+    private static void modificarProducto(Scanner scanner, ArrayList<String> productos, ArrayList<Integer> cantidades, ArrayList<Double> precios) {
+        System.out.println("Ingrese el número del producto a modificar: ");
+        mostrarProductos(productos);
+        int index = scanner.nextInt() - 1;  // Se resta 1 para usarlo como índice
+
+        if (index >= 0 && index < productos.size()) {
+            System.out.println("Ingrese el nuevo nombre del producto (actual: " + productos.get(index) + "): ");
+            String nuevoNombre = scanner.next();
+            System.out.println("Ingrese la nueva cantidad del producto (actual: " + cantidades.get(index) + "): ");
+            int nuevaCantidad = scanner.nextInt();
+            System.out.println("Ingrese el nuevo precio del producto (actual: " + precios.get(index) + "): ");
+            double nuevoPrecio = scanner.nextDouble();
+
+            productos.set(index, nuevoNombre);
+            cantidades.set(index, nuevaCantidad);
+            precios.set(index, nuevoPrecio);
+
+            System.out.println("Producto modificado exitosamente");
+        } else {
+            System.out.println("Número de producto inválido");
+        }
+    }
+
+    // Esta es la función para mostrar el inventario completo en formato de tabla
+    private static void mostrarInventario(ArrayList<String> productos, ArrayList<Integer> cantidades, ArrayList<Double> precios) {
+        if (productos.isEmpty()) {
+            System.out.println("El inventario está vacío");
+            return;
+        }
+
+        double valorTotalInventario = 0;
+
+        // Aca es para mostrar como va ser la tabla del inventario
+        System.out.println("\nDetalles del inventario:");
+        System.out.printf("%-20s %-10s %-15s %-15s\n", "Producto", "Cantidad", "Precio Unitario", "Precio Total");
+        System.out.println("--------------------------------------------------------------");
+
+        for (int i = 0; i < productos.size(); i++) {
+            double valorTotalProducto = cantidades.get(i) * precios.get(i);
+            System.out.printf("%-20s %-10d %-15.2f %-15.2f\n", productos.get(i), cantidades.get(i), precios.get(i), valorTotalProducto);
+            valorTotalInventario += valorTotalProducto;
+        }
+
+        // Muestra el valor total del inventario
+        System.out.println("--------------------------------------------------------------");
+        System.out.printf("%-20s %-10s %-15s %-15.2f\n", "Total Inventario", "", "", valorTotalInventario);
+    }
+
+    // Esta es la fcunción auxiliar para mostrar la lista de productos con índices
+    private static void mostrarProductos(ArrayList<String> productos) {
+        for (int i = 0; i < productos.size(); i++) {
+            System.out.println((i + 1) + ". " + productos.get(i));
+        }
+    }
+}
